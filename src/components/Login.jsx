d9047d3a48feeb88b1b8ed24f815a91eae85ac7b
Kuldeep Singh Tomar <kuldeep31singhtomar@gmail.com>
2023-11-06 05:05:55 +0530

    fixed few bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -49,7 +49,7 @@ function Login() {
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form className="mt-8" onSubmit={handleSubmit}>
+        <form className="mt-8" onSubmit={handleSubmit(login)}>
           <div className="space-y-5">
             <Input
               label="Email:"
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./Container/Container";
-import LogoutBtn from "./Header/Logoutbtn";
 import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
@@ -13,13 +12,13 @@ import PostCard from "./PostCard";
 import Select from "./Select";
 import Signup from "./Signup";
 import Login from "./Login";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
   Header,
   Footer,
   Logo,
   Container,
-  LogoutBtn,
   Button,
   Input,
   RTE,
@@ -30,5 +29,5 @@ export {
   Select,
   Signup,
   Login,
-
+  LogoutBtn
 };
